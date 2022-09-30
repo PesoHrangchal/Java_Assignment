4e290877a7014a0293eac6d0fbc138e4ac38a228
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-09-30 14:50:12 +0530

    Add files via upload

---
+++ 30-09-22/2/MyProgram.java
@@ -0,0 +1,9 @@
+/*Problem Statement:
+Fix the compilation error and invoke the method printMessage() of DisplayMessage class.*/
+
+public class MyProgram{
+	public static void main(String arg[]){
+	      DisplayMessage program=new DisplayMessage();
+	      program.printMessage();
+	   }
+}
